e239aa82ddfc0f7b2f8d577dc189d3198d8b0c2e
mark9134 <mark9134@gmail.com>
2018-12-19 09:51:37 -0500

    updated scratch.js

--- scratch.js
+++ scratch.js
@@ -1,13 +1,70 @@
 // ip address as string
 
-var ip_string = "192.168.0.1";
-var octals = ip_string.split('.');
-console.log(octals);
+var IpAddress = function(ip, mask) {
+    this.ip = ip;
+    this.netmask = mask;
+};
 
-//update ip_string html iwth var
-document.querySelector(".ip").textContent = ip_string;
-
-//update html octals
-for(x=0;x < octals.length;x++) {
-    document.querySelector('#o' + (x+1)).textContent = parseInt(octals[x]).toString(2);
+IpAddress.prototype.ipToArray = function() {
+    return (this.ip.split('.'));
 }
+
+function updateTable (rowType) {
+   if (rowType === 'binary') {
+        return function(server) {
+            var oArr = server.ipToArray();
+            var arrRes = [];
+            var str = '';
+            for (var x = 0; x < oArr.length; x++) {
+                var o = parseInt(oArr[x]).toString(2);
+                while (o.length < 8) {
+                    o = '0' + o;
+                }
+                arrRes.push(o);
+            }
+            //return(arrRes);
+            appendRow(arrRes, rowType);
+        }
+            
+    }
+    else if (rowType === 'octal') {
+        return function(server) {
+            var octalArr = server.ipToArray();
+            appendRow(octalArr, 'octal');
+
+        }
+    }
+
+};
+
+function appendRow (dataArr, type) {
+    var table = document.querySelector(".iptable");
+    if (type === 'binary') {
+        var row = table.insertRow(-1);
+        row.className = "binary-row";
+    } 
+    else if (type === 'octal') {
+        var row = table.insertRow(-1);
+        row.className = "octal-row";
+    }
+    for (var x = 0; x< dataArr.length; x++) {
+        cell = row.insertCell(x);
+        cell.innerHTML = dataArr[x];
+    }
+};
+
+var addBinaryRow = updateTable('binary');
+var addOctalRow = updateTable('octal');
+
+router = new  IpAddress('192.168.0.3', '255.255.255.0');
+macbook = new IpAddress('192.168.1.13','255.255.0.0');
+google = new  IpAddress('8.8.8.8', '255.255.255.240');
+
+var serverArray = [router, macbook, google];
+
+for (x = 0; x < serverArray.length; x++) {
+    addOctalRow(serverArray[x]);
+    addBinaryRow(serverArray[x]);
+}
+
+
